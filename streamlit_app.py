c2ce0d30b5fd32087105731d4288eda5c7fb0169
yasmeenashrafyasmeenashraf11-hue <yasmeenashrafyasmeenashraf11@gmail.com>
2026-03-27 05:20:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
 
-# Page configuration
-st.set_page_config(
-    page_title="مرشد المراهقة الآمنة",
-    page_icon="👨‍👩‍👧‍👦",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
 
 # Show title and description
 st.title("💬 Chatbot")
